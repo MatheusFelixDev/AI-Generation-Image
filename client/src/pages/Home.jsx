6486b7ddbb21b6c63d79617912fd969f67cafeac
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-02-06 16:53:05 -0300

    deploy

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://server-ai-generation-image.vercel.app/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
