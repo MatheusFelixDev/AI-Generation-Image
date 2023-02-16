2d8027e8d25032fa027220ca81bf312794558add
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-02-16 09:21:39 -0300

    test

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -38,7 +38,7 @@ const CreatePost = () => {
     if (form.prompt && form.resolution) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://ai-generation-image.vercel.app/api/v1/dalle', {
+        const response = await fetch('https://server-ai-generation-image.vercel.app/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -68,7 +68,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('https://ai-generation-image.vercel.app/api/v1/post', {
+        const response = await fetch('https://server-ai-generation-image.vercel.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
