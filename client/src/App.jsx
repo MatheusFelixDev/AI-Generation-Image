8ddb6e6fc15b73c5e1ccdfa96f0681a38dbbe845
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-02-24 21:41:22 -0300

    exact

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,8 +24,8 @@ const App = () => {
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe]
                       min-h-[calc(100vh-73px)]'>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/create-post' element={<CreatePost />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/create-post' element={<CreatePost />} />
         </Routes>
 
       </main>
